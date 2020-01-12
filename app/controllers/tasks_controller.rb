33208379053eca737b39a22dfe5cf55d6ec29e46
daisuke-hara0515 <wildlife402@gmail.com>
2020-01-12 15:09:07 +0900

    add gem(auto_link) modify code on show.html.slim

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,7 +17,7 @@ class TasksController < ApplicationController
 
   def update
     @task.update!(task_params)
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
+    redirect_to tasks_url, notice: "タスク「#{@task.name}」を更新しました。"
   end
 
   def create
