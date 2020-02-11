5e5b063b5a03930f7e7535c2aa1fcfeac662875f
daisuke-hara0515 <wildlife402@gmail.com>
2020-02-11 18:55:24 +0900

    if文追加 微修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -22,6 +22,11 @@ class TasksController < ApplicationController
 
   def create
     @task = current_user.tasks.new(task_params)
+
+    if params[:back].present?
+      render :new
+      return
+    end
     
     if @task.save
       logger.debug "task: #{@task.attributes.inspect}"
